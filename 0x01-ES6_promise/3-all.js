c7e16fa6659596d06f65844609b62a070c61dca7
faridlord10 <fahizoune10@gmail.com>
2024-03-06 12:18:02 -0800

    Task 03

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import {uploadPhoto, createUser} from './utils'
+
+export default async function handleProfileSignup(){
+	const { body } = await uploadPhoto();
+	const { firstName, lastName } = await createUser();
+	console.log(body,firstName, lastName)
+}
