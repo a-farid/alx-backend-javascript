828916d7db83e83c42680b1211a124bc662adc85
faridlord10 <fahizoune10@gmail.com>
2024-03-06 12:27:37 -0800

    04

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,7 @@
+// This is a test file to test the signUpUser function
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
+}
