766c4a7c697f0f6b2b8ef883498993e386d442f1
faridlord10 <fahizoune10@gmail.com>
2024-03-06 12:23:24 -0800

    task 03 eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  const { body } = await uploadPhoto();
-  const { firstName, lastName } = await createUser();
-  console.log(body, firstName, lastName);
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
