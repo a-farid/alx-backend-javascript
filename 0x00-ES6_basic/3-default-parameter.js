b72c7c69ad2389e65bef029828a27aa20e497895
faridlord10 <fahizoune10@gmail.com>
2024-03-06 13:57:19 -0800

    Task 03 eslint

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-const getSumOfHoodsi = (i, exp1989 = 89, exp2019 = 19) => i + exp1989 + exp2019;
-
-export default getSumOfHoodsi;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
